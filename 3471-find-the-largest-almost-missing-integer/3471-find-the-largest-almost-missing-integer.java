45739c2e5370c337a84c82a735ae7a8e6be03877
Vasu Agarwal <avasu769@gmail.com>
2026-08-18 20:56:42 +0530

    Time: 2 ms (72.63%), Space: 44.3 MB (94.39%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,34 +1,28 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer, Integer> ans = new HashMap<>();
-        for (int num : nums) {
-            ans.put(num, ans.getOrDefault(num, 0) + 1);
-        }
-        if (k == 1) {
+        HashMap<Integer,Integer> ans = new HashMap<>();
+        for(int num : nums) ans.put(num,ans.getOrDefault(num,0)+1);
+        if(k == 1) {
             int max = -1;
-            for (int num : nums) {
-                if (ans.get(num) == 1) {
-                    max = Math.max(max,num);
-                }
+            for(int num : nums){
+                if(ans.get(num) == 1) max = Math.max(max,num);
             }
             return max;
-        } else if (k == nums.length) {
+        }else if(k == nums.length){
             return large(nums);
-        } else {
+        }else{
             int first = nums[0];
-            int second = nums[nums.length - 1];
-             int firstValue = ans.get(first) == 1 ? first : -1;
-             int lastValue = ans.get(second) == 1 ? second : -1;
-             return Math.max(firstValue,lastValue);
+            int second = nums[nums.length-1];
+            int firstValue = ans.get(first) == 1 ? first :-1;
+            int secondValue = ans.get(second) == 1?second :-1;
+            return Math.max(firstValue,secondValue);
         }
     }
-
-    private int large(int[] nums) {
-        int max = 0;
-        for (int num : nums) {
-            if (num > max)
-                max = num;
-        }
-        return max;
+    private int large(int [] nums){
+    int max = 0;
+    for(int n : nums){
+        if(n>max) max=n ;
+    }
+    return max;
     }
 }
